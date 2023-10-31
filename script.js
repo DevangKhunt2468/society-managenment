432dadefc5c353d07421d4753439e421dff07064
DevangKhunt2468 <149499532+DevangKhunt2468@users.noreply.github.com>
2023-10-31 19:34:45 +0530

    added about html web page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.options-btn').addEventListener('click', function () {
+    const optionsMenu = document.querySelector('.options-menu');
+    optionsMenu.style.display = optionsMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+document.querySelector('.header-button').addEventListener('click', function () {
+    const optionsMenu = document.querySelector('.option');
+    optionsMenu.style.display = optionsMenu.style.display === 'block' ? 'none' : 'block';
+});
